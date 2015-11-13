c07b293e68e327c41f891e60b2c7fb60914823f0
Artur Otrzonsek <artur.otrzonsek@capgemini.com>
2015-11-13 14:00:07 +0100

    query dsl added #2

--- src/main/java/com/capgemini/spring/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/capgemini/spring/repository/impl/BookRepositoryImpl.java
@@ -2,7 +2,12 @@ package com.capgemini.spring.repository.impl;
 
 import com.capgemini.spring.model.BookEntity;
 import com.capgemini.spring.model.BookSearchCriteria;
+import com.capgemini.spring.model.QBookEntity;
 import com.capgemini.spring.repository.SearchByCriteriaRepository;
+import com.mysema.query.BooleanBuilder;
+import com.mysema.query.jpa.impl.JPAQuery;
+import com.mysema.query.types.Predicate;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,6 +20,20 @@ public class BookRepositoryImpl implements SearchByCriteriaRepository {
 
     @Override
     public List<BookEntity> findBySearchCriteria(BookSearchCriteria bookSearchCriteria) {
-        return null;
+        QBookEntity bookEntity = QBookEntity.bookEntity;
+        JPAQuery query = new JPAQuery(entityManager).from(bookEntity);
+
+        if (bookSearchCriteria != null) {
+            BooleanBuilder whereBuilder = new BooleanBuilder();
+            if (StringUtils.hasText(bookSearchCriteria.getTitle())) {
+                whereBuilder.and(bookEntity.title.equalsIgnoreCase(bookSearchCriteria.getTitle()));
+            }
+            if (StringUtils.hasText(bookSearchCriteria.getAuthor())) {
+                whereBuilder.and(bookEntity.authors.containsIgnoreCase(bookSearchCriteria.getAuthor()));
+            }
+            query.where(whereBuilder);
+        }
+
+        return query.list(bookEntity);
     }
 }
